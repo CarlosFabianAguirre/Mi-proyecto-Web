6815f81127d4b1099bbe9f5b1b1f65bcaaaa6e11
CarlosFabianAguirre <carlosfabianaguirre@hotmail.com>
2022-05-16 22:17:22 -0300

    primer commit del trabajo

---
+++ hamburguesa.js
@@ -0,0 +1,23 @@
+
+console.log("hola mundo");
+//quiero que cuando clikceo el boton de menu suceda algo 
+//por eso java escrip tiene que identificar el boton ,, con id en html
+//boton.addEventListener() significa que escuche los eventos que tengan el boton ,, en este caso
+//el click
+//()=>{ significa entonces sucede esto}
+//quiero que cuando se le de click  al boton se le agregue la clase mostrar al menu      
+//nav.classList.add
+//add: agregar clases
+//remove:elminar clases
+//toggle:agregar yo quitar una clase
+boton.addEventListener("click",()=>{
+    //todod de aca para abajo todo lo que va a suceder cuando se le de click al boton
+    console.log("hicistes click");
+    nav.classList.toggle("mostrar");
+});
+
+boton1.addEventListener("click",()=>{
+    //todod de aca para abajo todo lo que va a suceder cuando se le de click al boton
+    console.log("hicistes click");
+    listafinal.classList.toggle("mostrar1");
+});
